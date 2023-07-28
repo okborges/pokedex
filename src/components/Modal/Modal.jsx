bfca40148e24d9559f0a16fde5d23e77b0d17b1f
deennys <dennys.a@partner.samsung.com>
2023-07-28 09:16:09 -0300

    update

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import ReactModal from 'react-modal'
 
 export default function Modal({ modalIsOpen, closeModal, pokemon }) {
+	const [shinyPokemon, setShinyPokemon] = useState('normal')
+	const [images, setImages] = useState()
+	const [isActive, setIsActive] = useState()
+
+	function tradeShiny() {
+		handleClick()
+		if (isActive) {
+			setShinyPokemon('shiny')
+		} else {
+			setShinyPokemon('normal')
+		}
+	}
+
+	const handleClick = () => {
+		setIsActive(!isActive)
+	}
+
+	useEffect(() => {
+		setShinyPokemon('normal')
+		setImages(pokemon.image?.[shinyPokemon].two)
+	}, [])
+
+	useEffect(() => {
+		if (shinyPokemon === 'normal') {
+			setImages(pokemon.image?.[shinyPokemon].two)
+		} else {
+			setImages(pokemon.image?.[shinyPokemon].two)
+		}
+	}, [shinyPokemon])
+
 	return (
 		<ReactModal
 			isOpen={modalIsOpen}
@@ -12,10 +43,25 @@ export default function Modal({ modalIsOpen, closeModal, pokemon }) {
 			<p>#{pokemon.id}</p>
 			<span>generation {pokemon.generation}</span>
 			<h1>{pokemon.name}</h1>
-			<img src={pokemon.image?.normal.two} alt="" />
+			<img
+				onMouseOver={() =>
+					setImages(pokemon.image?.[shinyPokemon].gif)
+				}
+				onMouseLeave={() =>
+					setImages(pokemon.image?.[shinyPokemon].two)
+				}
+				src={images}
+				alt=""
+				width={400}
+				height={400}
+			/>
+			<button
+				className={`box ${!isActive ? 'active' : ''}`}
+				onClick={tradeShiny}
+			/>
 			<p>Specie: {pokemon.specie}</p>
 			<p>Descripition: {pokemon.description}</p>
-			
+
 		</ReactModal>
 	)
 }
